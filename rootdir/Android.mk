691f6349d33ed0faa3330d7c0188ba765707b534
Bruno Martins <bgcngm@gmail.com>
2018-02-07 23:38:12 +0000

    m8d: Move device-specific .rc files to /vendor/init/etc/hw
    Following main change in the msm8974-common repo,
    as well as the intermediate change in the m8-common repo.
    
    Change-Id: I7a003cec04d85f66cfc7791b0b0fd0665f11fe89

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE       := init.variant.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.variant.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 endif
