0fb15d342d0ac5785a54b2d37245b9d56b82ad20
Bruno Martins <bgcngm@gmail.com>
2021-03-07 21:42:33 +0000

    m8d: Adapt to common tree soong namespace
    Change-Id: I7941032fd0bcc5737d5e0f40ed7fba79aace4f96

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
-#               2017,2020 The LineageOS Project
+#               2017,2020-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -55,7 +55,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3087007744
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 11140071424
 
 # Vendor Init
-TARGET_INIT_VENDOR_LIB := libinit_m8d
+TARGET_INIT_VENDOR_LIB := //device/htc/m8d:libinit_m8d
 TARGET_RECOVERY_DEVICE_MODULES += libinit_m8d
 
 # Inherit from the proprietary version
